b18c6628f62599e29f9fddd9c4bb23afc17c7127
Stella Cannefax <stella@unity3d.com>
2020-08-09 16:02:51 -0700

    minor release cleanup

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,6 +1,6 @@
 //need this to make sure the popup shows
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+    chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
