239ab4caf5c8de8231c8db9b1dae1ff51eca36bc
Stella Cannefax <stella@unity3d.com>
2020-08-09 14:04:02 -0700

    better somment in background page

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,9 +1,4 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-//var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-//});
-
-//example of using a message handler from the inject scripts
+//need this to make sure the popup shows
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
