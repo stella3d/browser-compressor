429c20f26d4cdc7d26fe94e961ba7e35516f4fbb
Stella Cannefax <stella@unity3d.com>
2020-08-12 23:58:02 -0700

    remove unused background script

--- src/bg/background.js
+++
@@ -1,6 +0,0 @@
-// (seemingly) needed this to make sure the popup shows ??
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    chrome?.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
